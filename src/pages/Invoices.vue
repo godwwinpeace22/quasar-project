1abb2e9eeab178e33962ae1a9087c2d9f919cd63
Godwin Gabriel <godwinpeace22@gmail.com>
2022-09-27 00:59:37 +0100

    work on responsiveness

--- src/pages/Invoices.vue
+++ src/pages/Invoices.vue
@@ -16,38 +16,39 @@
         :class="$q.screen.gt.xs ? 'q-pa-lg' : 'q-pa-sm'"
         style="border-radius: 15px"
       >
-        <q-list class="q-mb-md">
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
-              </q-avatar>
-            </q-item-section>
+        <div class="row justify-between">
+          <q-list class="q-mb-md">
+            <q-item class="q-pl-none">
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section top class="col-2 gt-sm">
-              <q-item-label class="q-mt-sm text-weight-bold text-capitalize">{{
-                customer?.name
-              }}</q-item-label>
-              <q-item-label caption lines="1">
-                {{ customer?.email }}
-              </q-item-label>
-            </q-item-section>
-
-            <q-item-section side style="width: 100%">
-              <q-item-label lines="1">
-                <span class="text-grey-8">
-                  {{ customer?.address?.street }}</span
+              <q-item-section top class="col-12 col-sm-2">
+                <q-item-label
+                  class="q-mt-sm text-weight-bold text-capitalize"
+                  >{{ customer?.name }}</q-item-label
                 >
-              </q-item-label>
-              <q-item-label caption lines="1">
-                {{ customer?.address.city }}
-              </q-item-label>
-              <q-item-label caption lines="1">
-                {{ customer?.address?.state }}, {{ customer?.address?.country }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
+                <q-item-label caption>
+                  {{ customer?.email }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+
+          <div :class="$q.screen.lt.sm ? 'q-mb-md' : ''">
+            <div>
+              <span class="text-grey-8"> {{ customer?.address?.street }}</span>
+            </div>
+            <div class="text-grey-8">
+              {{ customer?.address?.city }}
+            </div>
+            <div class="text-grey-8">
+              {{ customer?.address?.state }}, {{ customer?.address?.country }}
+            </div>
+          </div>
+        </div>
 
         <q-card
           flat
@@ -95,7 +96,7 @@
           </q-card-section>
         </q-card>
 
-        <div class="row q-mt-lg q-mb-md justify-between">
+        <div class="row q-mt-lg q-mb-md justify-between align-center">
           <div>
             <div class="text-weight-bold">Item Detail</div>
             <div class="text-sm text-grey-8">Type Item for Hours item.</div>
@@ -106,61 +107,101 @@
               color="grey-4"
               text-color="primary"
               unelevated
-              icon="camera_enhance"
               label="Customize"
+              no-caps
               size="md"
             />
           </div>
         </div>
         <q-separator />
-        <div
-          v-for="item in invoice?.items"
-          :key="item.id"
-          class="row q-gutter-lg q-my-md"
-        >
-          <div class="col-12 col-sm-5 text-grey-8">
-            <div class="text-grey-8 q-mb-lg">Item Name</div>
-            <q-input outlined v-model="itemName" bg-color="" />
+        <div class="row q-my-sm q-gutter-lg" v-if="$q.screen.gt.xs">
+          <div class="col-12 col-sm-4 text-grey-8">
+            <div class="text-grey-8">Item Name</div>
           </div>
-          <div class="col-12 col-sm-1 text-grey-8">
-            <div class="text-grey-8 q-mb-lg">Hours</div>
-            <q-input outlined v-model="hours" bg-color="" />
+          <div class="col-4 col-sm-1 text-grey-8">
+            <div class="text-grey-8">Hours</div>
           </div>
-          <div class="col-12 col-sm-1 text-grey-8">
-            <div class="text-grey-8 q-mb-lg">Rate/hr</div>
-            <q-input
-              outlined
-              v-model="rate"
-              type="number"
-              prefix="$"
-              bg-color=""
-            />
+          <div class="col-4 col-sm-1 text-grey-8">
+            <div class="text-grey-8">Rate/hr</div>
           </div>
-          <div class="col-12 col-sm-1 text-grey-8">
-            <div class="text-grey-8 q-mb-lg">Tax</div>
-            <q-input
-              outlined
-              v-model="tax"
-              type="number"
-              prefix="$"
-              bg-color=""
-            />
+          <div class="col-4 col-sm-1 text-grey-8">
+            <div class="text-grey-8">Tax</div>
           </div>
           <div class="col text-grey-8">
-            <div class="text-grey-8 q-mb-lg">Line Total</div>
-            <q-input
-              outlined
-              v-model="total"
-              type="number"
-              prefix="$"
-              bg-color=""
-              color="black"
-            />
+            <div class="text-grey-8">Line Total</div>
+          </div>
+        </div>
+        <q-separator v-if="$q.screen.gt.xs" />
+
+        <div v-for="item in items" :key="item.id">
+          <div>
+            <div
+              class="row q-my-md"
+              :class="$q.screen.gt.xs ? 'q-gutter-lg' : 'q-gutter-xs'"
+            >
+              <div class="col-12 col-sm-4 text-grey-8">
+                <q-input
+                  outlined
+                  :label="$q.screen.lt.sm ? 'Name' : ''"
+                  v-model="item.name"
+                  bg-color=""
+                />
+              </div>
+              <div class="col-4 col-sm-1 text-grey-8">
+                <q-input
+                  outlined
+                  v-model="item.hours"
+                  bg-color=""
+                  :label="$q.screen.lt.sm ? 'Hours' : ''"
+                />
+              </div>
+              <div class="col-4 col-sm-1 text-grey-8">
+                <q-input
+                  outlined
+                  :label="$q.screen.lt.sm ? 'Rate' : ''"
+                  v-model="item.rate"
+                  type="number"
+                  prefix="$"
+                  bg-color=""
+                />
+              </div>
+              <div class="col-4 col-sm-1 text-grey-8">
+                <q-input
+                  outlined
+                  :label="$q.screen.lt.sm ? 'Tax' : ''"
+                  v-model="item.tax"
+                  type="number"
+                  prefix="$"
+                  bg-color=""
+                />
+              </div>
+              <div class="col text-grey-8">
+                <q-input
+                  outlined
+                  :label="$q.screen.lt.sm ? 'Total' : ''"
+                  v-model="item.total"
+                  type="number"
+                  prefix="$"
+                  bg-color=""
+                  color="black"
+                />
+              </div>
+              <div class="col text-grey-8">
+                <q-btn
+                  class="q-mt-sm"
+                  unelevated
+                  round
+                  icon="add"
+                  color="primary"
+                ></q-btn>
+              </div>
+            </div>
+            <q-separator />
           </div>
         </div>
 
         <div class="row q-my-lg">
-          <div class="col-12 col-sm-5">
+          <div class="col-12 col-sm-4">
             <q-input
               outlined
               v-model="description"
@@ -171,7 +212,7 @@
         </div>
         <q-separator />
         <div class="row q-mt-md q-gutter-sm justify-between">
-          <div class="col-12 col-sm-5 text-weight-bold">
+          <div class="col-12 col-sm-6 text-weight-bold">
             <div class="row">
               <div class="col">Payment Method</div>
 
@@ -194,24 +235,24 @@
                 </div>
                 <div class="q-mb-sm">
                   <span class="text-grey-8 text-weight-light"
-                    >Account Name:</span
-                  >
+                    >Account Name:
+                  </span>
                   <span class="text-weight-medium">
                     {{ invoice?.paymentMethod?.accountName }}</span
                   >
                 </div>
                 <div class="q-mb-sm">
                   <span class="text-grey-8 text-weight-light"
-                    >Account Number:</span
-                  >
+                    >Account Number:
+                  </span>
                   <span class="text-weight-medium">
                     {{ invoice?.paymentMethod?.accountNumber }}</span
                   >
                 </div>
                 <div class="q-mb-sm">
                   <span class="text-grey-8 text-weight-light"
-                    >Routing Number:</span
-                  >
+                    >Routing Number:
+                  </span>
                   <span class="text-weight-medium">
                     {{ invoice?.paymentMethod?.routingNumber }}</span
                   >
@@ -220,14 +261,21 @@
             </div>
           </div>
           <!-- <div class="col-1 text-grey-8">Hours</div> -->
-          <div class="col-12 col-sm-3">
+          <div class="col-12 col-sm-4">
             <div class="row justify-between q-mb-md">
               <div class="col text-weight-bold">Sub Total</div>
-              <div class="col text-right text-weight-bold">$42,000</div>
+              <div class="col text-right text-weight-bold">${{ total }}</div>
             </div>
             <div class="row justify-between q-mb-md">
               <div class="col text-grey-8">Discount</div>
-              <div class="col-2 text-right text-weight-medium">$0.00</div>
+              <div class="col-2 text-right text-weight-medium">
+                ${{
+                  discount.toLocaleString('en', {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })
+                }}
+              </div>
             </div>
             <div class="row justify-between">
               <div class="col text-grey-8">Total Tax</div>
@@ -236,7 +284,7 @@
             <q-separator class="q-mb-md q-mt-xl" />
             <div class="row justify-between">
               <div class="text-weight-bold">Total Amount</div>
-              <div class="text-weight-bold">$4,800.00</div>
+              <div class="text-weight-bold">${{ total }}</div>
             </div>
           </div>
         </div>
@@ -246,43 +294,72 @@
 </template>
 
 <script lang="ts">
-import { Invoice, Customer } from 'components/models';
+import { Invoice, Customer, InvoiceItem } from 'components/models';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'InvoicesPage',
   components: {},
-  setup() {
-    let invoice = ref<Invoice>();
-    let customer = ref<Customer>();
-    let hours = ref(120);
-    let rate = ref(0);
-    let tax = ref(0);
-    let total = ref(0);
+  data() {
+    return {
+      invoice: [] as unknown as Invoice,
+      customer: [] as unknown as Customer,
+      items: [
+        { id: 1, name: '', hours: 0, rate: 0, tax: 0, total: 0 },
+      ] as unknown as InvoiceItem[],
+      description: '',
+      discount: 0,
+    };
+  },
 
+  computed: {
+    total() {
+      const sum = this.invoice?.items?.reduce(
+        (prev, curr) => curr?.total + prev,
+        0
+      );
+
+      const options = {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      };
+
+      return (sum - this.discount)?.toLocaleString('en', options);
+    },
+  },
+  mounted() {
     fetch('/api/invoices/1')
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
-        invoice.value = json.invoice;
+        this.invoice = json.invoice;
+        this.items = json?.invoice?.items;
 
         fetch('/api/customers/' + json.invoice.customerId)
           .then((res) => res.json())
           .then((json) => {
             console.log(json);
-            customer.value = json.customer;
+            this.customer = json.customer;
           });
       });
-    return {
-      invoice,
-      customer,
-      itemName: ref('Payment Project - Moonlight Mobile Design'),
-      hours,
-      rate,
-      tax,
-      total,
-      description: ref(''),
-    };
+  },
+  setup() {
+    // let invoice = ref<Invoice>();
+    // let customer = ref<Customer>();
+    // fetch('/api/invoices/1')
+    //   .then((res) => res.json())
+    //   .then((json) => {
+    //     invoice.value = json.invoice;
+    //     fetch('/api/customers/' + json.invoice.customerId)
+    //       .then((res) => res.json())
+    //       .then((json) => {
+    //         customer.value = json.customer;
+    //       });
+    //   });
+    // return {
+    //   invoice,
+    //   customer,
+    // };
   },
 });
 </script>
